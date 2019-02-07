af42ce7c55718674158e11a76096dfe5ef77aa7b
Alleluid <alleluid@users.noreply.github.com>
2019-02-06 18:09:59 -0800

    Modify into persistent window

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import PySimpleGUI as sg
 
 
 def main():
-    layout = [[sg.Text('my one-shot window')],
-              [sg.InputText()],
-              [sg.Submit(), sg.Cancel()]]
+    layout = [[sg.Text('Persistent Window')],
+              [sg.Input(do_not_clear=True)],
+              [sg.Button('Read'), sg.Exit()]]
 
     window = sg.Window('Window Title').Layout(layout)
 
-    event, values = window.Read()
-    window.Close()
+    while True:
+        event, values = window.Read()
+        if event is None or event == 'Exit':
+            break
+        print(values[0])
 
-    print(values[0])
+    window.Close()
 
 
 if __name__ == '__main__':
