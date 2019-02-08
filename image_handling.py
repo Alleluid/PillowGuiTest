2e9584314ad15b5cc29b3aaa6c4cff955ec8e01f
Alleluid <alleluid@users.noreply.github.com>
2019-02-07 16:06:56 -0800

    Remove test code, Fix image viewer

--- image_handling.py
+++ image_handling.py
@@ -1,12 +1,36 @@
+import io
+
 from PIL import Image
-from PIL.ImageTk import BitmapImage
 
 
 def processing(filename: str):
+    _MIN_SIZE = 512
+
     img: Image.Image = Image.open(filename)
-    return BitmapImage(img)
+    bio = io.BytesIO()
+    img_modded = img.copy()
+
+    ix, iy = img_modded.size
+    if min(ix, iy) < _MIN_SIZE:
+        multi = 2
+        new_x, new_y = ix, iy
+        while min(new_x, new_y) < _MIN_SIZE:
+            new_x = ix * multi
+            new_y = iy * multi
+            multi += 1
+        print(f"multi was {multi}")
+        print(f"image size is now {new_x}, {new_y}")
+        img_modded = img_modded.resize((new_x, new_y), Image.NEAREST)
+
+    img_modded.save(bio, format="PNG")
+    return bio.getvalue()
+
+def img_obj_to_bytes(image: Image) -> bytes:
+    bio = io.BytesIO()
+    image.save(bio, format="PNG")
+    return bio.getvalue()
 
 
 if __name__ == '__main__':
     filename = r"D:\Users\milot\Pictures\alleluid_leaf_badass.png"
-    print(processing(filename))
+    print(type(processing(filename)))
--- main.py
+++ main.py
@@ -3,35 +3,6 @@ import image_handling
 
 
 def main():
-    layout = [[sg.Text('Change Look | Currently:'), sg.Text('', key='_OUTPUT_')],
-              [sg.Input(do_not_clear=True, key='_IN_')],
-              [sg.Button('Change'), sg.Exit()]]
-
-    window = sg.Window('Window Title').Layout(layout)
-
-    while True:
-        event, values = window.Read()
-        print(event, values)
-        if event is None or event == 'Exit':
-            break
-        if event == 'Change':
-            sg.ChangeLookAndFeel(values['_IN_'])
-            window.FindElement('_OUTPUT_').Update(values['_IN_'])
-
-    window.Close()
-
-
-def get_image_gui():
-    layout = [
-        [sg.Text("Select image file:")],
-        [sg.InputText(), sg.FileBrowse()],
-        [sg.Submit(), sg.Exit()]
-    ]
-
-    return sg.Window("Open Image").Layout(layout).Read()
-
-
-def test_image_display():
     event, (source_filename,) = get_image_gui()
 
     img_data = image_handling.processing(source_filename)
@@ -48,5 +19,15 @@ def test_image_display():
     window.Close()
 
 
+def get_image_gui():
+    layout = [
+        [sg.Text("Select image file:")],
+        [sg.InputText(), sg.FileBrowse()],
+        [sg.Submit(), sg.Exit()]
+    ]
+
+    return sg.Window("Open Image").Layout(layout).Read()
+
+
 if __name__ == '__main__':
-    test_image_display()
+    main()
